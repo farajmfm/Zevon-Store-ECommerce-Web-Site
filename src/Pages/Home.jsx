e055a9b777f4d21df9fd268d1513d42c83d4f82a
mohomed faraj <86347430+farajmfm@users.noreply.github.com>
2023-01-18 19:27:47 +0530

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Announcement from '../Components/Announcement'
+import Navbar from '../Components/Navbar'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <Announcement />
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
+
