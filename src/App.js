43020e539e7b1834c35bb853bb6071427b06ac65
mohomed faraj <86347430+farajmfm@users.noreply.github.com>
2023-01-21 11:41:11 +0530

    productList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Home from "./Pages/Home";
+//import Home from "./Pages/Home";
+import ProductList from "./Pages/ProductList";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/*<Home />*/}
+      <ProductList />
     </div>
   );
 }
